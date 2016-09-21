81774bc6a6ebdd76efeaef305e82ab32215c130a
Rik Boeykens <Rik Boeykens>
2016-09-21 13:56:23 +0100

    Added materialize

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
 $(function() {
 
     var $optionsContainer = $('#options-container');
+    var $resultModal = $('#result-modal');
 
     var addLine = function(){
+        var newOptionCount = $optionsContainer.find('.option').length + 1;
+
         var $newLine = $('#templates > #option-row-template').html()
                         .replace('option-placeholder', 'option')
-                        .replace('delete-line-placeholder', 'delete-line');
+                        .replace('delete-line-placeholder', 'delete-line')
+                        .replace(/id-placeholder/g, 'option-' + newOptionCount)
+                        .replace('text-placeholder', 'Option ' + newOptionCount);
         
         var $lastLine = $optionsContainer.find('.option').last();
         $lastLine.after($newLine);
@@ -15,6 +20,15 @@ $(function() {
         $newLastLine.find('.delete-line').click(deleteLine);
     }
 
+    var updateOptionRowIds = function(){
+        $optionsContainer.find('.option').each(function(index){
+            index++;
+            $(this).find('input').attr('id', 'option-' + index);
+            $(this).find('label').attr('for', 'option-' + index);
+            $(this).find('label').text('Option ' + index);
+        });
+    }
+
     var deleteLine = function(){
         if($('.option').length <= 2){
             return;
@@ -22,12 +36,21 @@ $(function() {
         var $line = $(this).closest('.option');
 
         $line.remove();
+
+        updateOptionRowIds();
+
+    }
+
+    var showResult = function(result){
+        $resultModal.find('h3').text(result);
+        $resultModal.openModal();
     }
 
     var decide = function(){
         var options = $('.option').map(function(){return $(this).find('input').val()});
         var randomOption = options[Math.floor(Math.random() * options.length)];
-        alert(randomOption);
+
+        showResult(randomOption);
     }
 
     $optionsContainer.find('#add-option').click(addLine);
